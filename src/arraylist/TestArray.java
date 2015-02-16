aa7cadf0d5721469998e16f66d841fbaa0ab5c07
Simmigon-flagg <simmigon.flagg@gmail.com>
2015-02-16 10:59:18 -0500

    This was deleted but I forget to commit.

--- src/arraylist/TestArray.java
+++ src/arraylist/TestArray.java
@@ -19,38 +19,5 @@ public class TestArray {
         //Use Default value inside consturcor
         ArrayList theArray = new ArrayList();
 
-        theArray.add("Simmigon");
-        theArray.add("Flagg");
-        theArray.add("Age: 34");
-        theArray.add("Simmigon");
-        theArray.add("Flagg");
-        theArray.add("Age: 34");
-        theArray.add("Simmigon");
-        theArray.add("Flagg");
-        theArray.add("Jovy");
-        theArray.add("Cortez");
-        theArray.add("Age: 23");
-
-
-        for (int i = 0; i < theArray.size(); i++) {
-
-            System.out.println(i + " " + theArray.get(i));
-
-        }
-        ArrayList theArrayPlace = new ArrayList();
-        theArrayPlace.add(0, "Simmigon");
-        theArrayPlace.add(2, "Flagg");
-        theArrayPlace.add(3, "Age");
-        theArrayPlace.add(3, "34");
-        theArrayPlace.add(1, "Daril");
-        theArrayPlace.add("Simmigon");
-
-
-
-        for (int i = 0; i < theArray.size(); i++) {
-            System.out.println(i + " " + theArrayPlace.get(i));
-        }
-        System.out.println(theArrayPlace.size());
-
     }
 }
